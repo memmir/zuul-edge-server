d8074a8a57bed0eff81950f81ae8da817a4e4b6a
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-04-13 14:49:27 +0300

    Updates.

--- src/main/java/com/mahmuttech/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
+++ src/main/java/com/mahmuttech/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -1,17 +1,39 @@
 package com.mahmuttech.stockmanagement.zuuledgeserver;
 
+import jakarta.ws.rs.HttpMethod;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@CrossOrigin(origins = "*", maxAge=3600)
-public class ZuulEdgeServerApplication {
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge=3600)
+public class  ZuulEdgeServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulEdgeServerApplication.class, args);
     }
+
+//    @Bean
+//    public CorsFilter corsFilter(){
+//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        final CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.setAllowedOrigins(Collections.singletonList("*"));
+//        config.setAllowedHeaders(Collections.singletonList("*"));
+//        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 
 }
