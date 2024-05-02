b6e3db48dd8b009789d37734721ade01ace4c557
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-02 11:39:23 +0300

    changed package name.

--- src/main/java/com/microshop/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
+++ src/main/java/com/microshop/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -1,20 +1,10 @@
-package com.mahmuttech.stockmanagement.zuuledgeserver;
+package com.microshop.stockmanagement.zuuledgeserver;
 
-import jakarta.ws.rs.HttpMethod;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
--- src/test/java/com/microshop/stockmanagement/zuuledgeserver/ZuulEdgeServerApplicationTests.java
+++ src/test/java/com/microshop/stockmanagement/zuuledgeserver/ZuulEdgeServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mahmuttech.stockmanagement.zuuledgeserver;
+package com.microshop.stockmanagement.zuuledgeserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
